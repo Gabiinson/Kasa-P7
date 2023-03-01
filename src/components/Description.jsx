88875c48fa3fce9ea546823bf0db881ae3944dd1
Bohaer Gabin <gabinson@outlook.fr>
2023-03-01 13:26:30 +0100

    init du fichier description, tag et host compris dedans

---
+++ src/components/Description.jsx
@@ -0,0 +1,37 @@
+import Collapse from "./Collapse";
+import Tag from "./Tag";
+import Host from "./Host";
+/**Rajouter Rating dans les import**/
+
+function Description ({
+    title, description, host, rating, location, equipments, tags,
+}) {
+    return (
+        <div className="info">
+            <div className="info1"> 
+                <div className="card1">
+                    <h1 className="title">{title}</h1>
+                    <p className="locate">{location}</p>
+                    <Tag tags={tags} />
+                </div>
+                <div className="card2">
+                    <Host host = {host} />
+                </div>
+            </div>
+            <div className="descEquip">
+                <section className="desc">
+                    <Collapse 
+                        title = {"Description"} 
+                        content = {description} />
+                </section>
+                <section className="equip">
+                    <Collapse 
+                        title = {"Equipements"}
+                        content = {equipments} />
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Description ;
